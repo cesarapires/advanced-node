ffac6555d5b008cfc1447ed093b73020699eb20b
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-06 22:40:07 -0300

    refactor: improve code quality

--- src/infraestructure/apis/facebook.ts
+++ src/infraestructure/apis/facebook.ts
@@ -1,6 +1,22 @@
 import { HttpGetClient } from '@/infraestructure/http'
 import { LoadFacebookUserApi } from '@/data/contracts/api'
 
+type AppToken = {
+  access_token: string
+}
+
+type DebugToken = {
+  data: {
+    user_id: string
+  }
+}
+
+type FacebookUser = {
+  id: string
+  name: string
+  email: string
+}
+
 export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
 
@@ -11,7 +27,17 @@ export class FacebookApi implements LoadFacebookUserApi {
   ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    const appToken = await this.httpGetClient.get({
+    const userFacebook = await this.getFacebookUser(params.token)
+
+    return {
+      facebookId: userFacebook.id,
+      name: userFacebook.name,
+      email: userFacebook.email
+    }
+  }
+
+  private async getAppToken (): Promise<AppToken> {
+    return this.httpGetClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_url: this.clientId,
@@ -19,28 +45,29 @@ export class FacebookApi implements LoadFacebookUserApi {
         grant_type: 'client_credentials'
       }
     })
+  }
 
-    const debugToken = await this.httpGetClient.get({
+  private async getDebugToken (clientToken: string): Promise<DebugToken> {
+    const appToken = await this.getAppToken()
+
+    return this.httpGetClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
-        input_token: params.token
+        input_token: clientToken
       }
     })
+  }
 
-    const userFacebook = await this.httpGetClient.get({
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  private async getFacebookUser (clientToken: string): Promise<FacebookUser> {
+    const debugToken = await this.getDebugToken(clientToken)
+
+    return this.httpGetClient.get({
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
         fields: ['id', 'name', 'email'].join(','),
-        access_token: params.token
+        access_token: clientToken
       }
     })
-
-    return {
-      facebookId: userFacebook.id,
-      name: userFacebook.name,
-      email: userFacebook.email
-    }
   }
 }
