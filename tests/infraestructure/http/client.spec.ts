0dce96ccc322dbcdca2e23a59e04bfbf3eb69681
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-07 13:04:10 -0300

    feat: ensure AxiosHttpClient rethrows if get throw

--- tests/infraestructure/http/client.spec.ts
+++ tests/infraestructure/http/client.spec.ts
@@ -46,5 +46,13 @@ describe('AxiosHttpClient', () => {
 
       expect(result).toEqual('any_data')
     })
+
+    it('should rethrow if get throw', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url: url, params: params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
