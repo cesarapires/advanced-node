0ca514616906acc39fbbad7176a7ceb1833609be
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-08 17:57:18 +0000

    feat: ensure PostregreeUserAccountRepository return undefinied if email does not exists

--- tests/infraestructure/postgrees/repository/user-account.spec.ts
+++ tests/infraestructure/postgrees/repository/user-account.spec.ts
@@ -53,6 +53,24 @@ describe('PostgreesUserAccountRepositpry', () => {
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+
+      await connection.close()
+    })
+
+    it('should return undefinied if not email exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+
+      await connection.synchronize()
+
+      const sut = new PostregreeUserAccountRepository()
+
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
     })
   })
 })
