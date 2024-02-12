94dbc9eb2d090b4b567a8736c77591ad81d7b980
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-12 12:16:27 +0000

    chore: configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FACEBOOK_CLIENT_ID ?? '2591539521014403',
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? '42712a7a64430b70d096d9109ad90d7e'
-  }
+  },
+  port: process.env.PORT ?? '8080'
 }
