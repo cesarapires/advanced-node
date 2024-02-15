d842732c7336b076cd06ab3bb4cf51fec46062e4
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-15 12:44:30 +0000

    chore: move ExpressRouter to its own file

--- tests/infraestructure/http/express-router.spec.ts
+++ tests/infraestructure/http/express-router.spec.ts
@@ -2,20 +2,7 @@ import { Request, Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { MockProxy, mock } from 'jest-mock-extended'
 import { Controller } from '@/application/controllers'
-
-class ExpressRouter {
-  constructor (private readonly controller: Controller) {}
-
-  async adapt (request: Request, response: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...request.body })
-
-    if (httpResponse.statusCode === 200) {
-      response.status(200).json(httpResponse.data)
-    } else {
-      response.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
-    }
-  }
-}
+import { ExpressRouter } from '@/infraestructure/http'
 
 describe('ExpressRouter', () => {
   let request: Request
