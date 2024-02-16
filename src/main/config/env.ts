fbb517f4a42dc5344d5ae155ea29d77dfe23bb98
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-16 22:25:51 +0000

    chore: move facebook_api config to dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,8 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FACEBOOK_CLIENT_ID ?? '2591539521014403',
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? '42712a7a64430b70d096d9109ad90d7e'
+    clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? ''
   },
   port: process.env.PORT ?? '3000',
-  jwtSecret: process.env.JWT_SECRETE ?? 'sdfçk3289njd'
+  jwtSecret: process.env.JWT_SECRET ?? '123456'
 }
