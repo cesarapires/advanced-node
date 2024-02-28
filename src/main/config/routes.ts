878c9d24990bbc7eba0f20844526a243127348e1
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-02-28 15:07:49 -0300

    fix: removes direct dependency on routes with route config

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,18 +1,13 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
 import { join } from 'path'
-import login from '@/main/routes/login'
 
 export const setupRoutes = (express: Express): void => {
   const router = Router()
-
   readdirSync(join(__dirname, '../routes'))
     .filter(file => !file.endsWith('.map'))
     .map(async file => {
-      (await import(`../routes/${file}`))
+      (await import(`../routes/${file}`)).default(router)
     })
-
-  login(router)
-
   express.use('/api', router)
 }
